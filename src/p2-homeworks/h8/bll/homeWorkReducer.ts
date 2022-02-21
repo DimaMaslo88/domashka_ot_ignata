8e04ff5a3c97c2529cac7c49246655b337bedeab
DimaMaslo88 <dmitrij-maslo@mail.ru>
2022-02-21 14:59:13 +0300

    -m "home"

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,54 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from "../HW8";
+
+type  GeneralType = sortUsersUpType|checkUserType| sortUsersDownType
+let initialState: UserType[]
+
+export const homeWorkReducer = (state: Array<UserType> = initialState, action: GeneralType): Array<UserType> => { // need to fix any
     switch (action.type) {
-        case 'sort': {
+        case "SORT_UP": {
             // need to fix
-            return state
+            const newState = [...state].sort((a, b) => {
+                if (a.name > b.name) return 1
+                else if(a.name <b.name) return -1
+                else return 0
+
+                }
+            )
+            return action.payload==="up"?newState:state
+            }
+        case "SORT_DOWN":{
+            const newState = [...state].sort((a, b) => {
+                    if (a.name > b.name) return -1
+                    else if(a.name <b.name) return 1
+                    else return 0
+
+                }
+            )
+            return action.payload==="down"?newState:state
         }
-        case 'check': {
-            // need to fix
-            return state
+
+
+
+        case "CHECK": {
+
+            return  state.filter(f=>f.age>=action.payload)// need to fix
+
+
         }
-        default: return state
+        default:
+            return state
     }
 }
+
+export type  sortUsersUpType = ReturnType<typeof sortUsersUpAC>
+
+export const sortUsersUpAC = () => ({ type: "SORT_UP", payload:"up"}as const)
+
+
+export type  sortUsersDownType = ReturnType<typeof sortUsersDownAC>
+export const sortUsersDownAC = () => ({ type: "SORT_DOWN", payload:"down"}as const)
+
+
+
+export type checkUserType = ReturnType<typeof checkUsersAC>
+export const checkUsersAC = () => ({type: "CHECK", payload:18}as const)
